6a7a9a6621eee41539666d3d444fd1ec6817c6bc
Martin Morek <martin.morek@gmail.com>
2021-06-10 15:08:21 +0200

    Added channel presence handling

--- java/ChatPub.java
+++ java/ChatPub.java
@@ -1,4 +1,5 @@
 AblyRealtime ably = new AblyRealtime("ABLY_API_KEY");
 
 Channel channel = ably.channels.get("room:tesla-fans");
+channel.presence.enter(null, null);
 channel.publish("msg", "{ \"user\": \"elon\", \"message\": \"Hello fans\" }");
--- java/ChatSub.java
+++ java/ChatSub.java
@@ -2,6 +2,13 @@ AblyRealtime ably = new AblyRealtime("ABLY_API_KEY");
 
 Channel channel = ably.channels.get("room:tesla-fans");
 
+channel.presence.subscribe(new Presence.PresenceListener() {
+    @Override
+    public void onPresenceMessage(PresenceMessage message) {
+        System.out.println(message.toString());
+    }
+});
+
 channel.subscribe(new Channel.MessageListener() {
     @Override
     public void onMessage(Message message) {
