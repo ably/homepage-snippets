c8e1cb04eb75f3cb309ed265b1d07df2c0e9ddea
Łukasz Śliwa <lukasz.sliwa@apptamers.com>
2021-06-15 22:57:34 +0200

    Hidden irrelevant code. Updated snippets. Removed require 'ably' and Ably::Realtime.new

--- ruby/iot_pub.rb
+++ ruby/iot_pub.rb
@@ -1,5 +1,2 @@
-require 'ably'
-
-ably = Ably::Realtime.new(key: ENV['ABLY_API_KEY'])
-channel = ably.channels.get 'device:rocket:1'
-channel.publish'action', { task: 'enable-thrusters' }
+channel = ably.channels.get('device:rocket:1')
+channel.publish('action', { task: 'enable-thrusters' })
--- ruby/iot_sub.rb
+++ ruby/iot_sub.rb
@@ -1,9 +1,6 @@
-require 'ably'
-
-ably = Ably::Realtime.new(key: ENV['ABLY_API_KEY'])
-channel = ably.channels.get 'device:rocket:1'
+channel = ably.channels.get('device:rocket:1')
 channel.subscribe do |payload|
   if payload.name == 'action'
-    mqtt.execute(payload.data)
+    mqtt.publish(payload.name, payload.data)
   end
 end
