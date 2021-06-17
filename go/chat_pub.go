521a21858acda1ddfd13d6c3752e93b642e682d7
sacOO7 <sachinshinde7676@gmail.com>
2021-06-17 19:47:38 +0530

    Updated code as per comments

--- go/chat_pub.go
+++ go/chat_pub.go
@@ -1,11 +1,14 @@
 client, err := ably.NewRealtime(ably.WithKey("ABLY_API_KEY"))
-channel := client.Channels.Get('room:tesla-fans')
-pErr := channel.Presence.Enter(context.Background(), UserName + " entered the channel")
-if pErr != nil {
-	err := fmt.Errorf("error with enter presence on the channel %w", pErr)
+if err != nil {
+	return err
+}
+channel := client.Channels.Get("room:tesla-fans")
+err := channel.Presence.Enter(context.Background(), UserName + " entered the channel")
+if err != nil {
+	err := fmt.Errorf("error with enter presence on the channel %w", err)
 	fmt.Println(err)
 }
-err = channel.Publish(context.Background(), 'msg', "{'user': 'elon', 'message': 'Hello fans'}")
+err = channel.Publish(context.Background(), "msg", map[string]string{"user": "elon", "message": "Hello fans"})
 if err != nil {
 	err := fmt.Errorf("error publishing to channel: %w", err)
 	fmt.Println(err)
--- go/multi_user_pub.go
+++ go/multi_user_pub.go
@@ -1,6 +1,9 @@
 client, err := ably.NewRealtime(ably.WithKey("ABLY_API_KEY"))
-channel := client.Channels.Get('doc:mars-launch-plan')
-err = channel.Publish(context.Background(), 'update', "{ 'pos': 'charAt', 'insert': 'Send Grimes first' }")
+if err != nil {
+	return err
+}
+channel := client.Channels.Get("doc:mars-launch-plan")
+err = channel.Publish(context.Background(), "update", map[string]string{ "pos": "charAt", "insert": "Send Grimes first" })
 if err != nil {
 	err := fmt.Errorf("error publishing to channel: %w", err)
 	fmt.Println(err)
