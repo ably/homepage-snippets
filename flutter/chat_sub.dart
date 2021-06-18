fc8aac57d4a5245028916e420acafc0a49cc2efb
Rohit R. Abbadi <tiholic@users.noreply.github.com>
2021-06-18 10:02:45 +0530

    formatting improvement for subscribe examples

--- flutter/chat_sub.dart
+++ flutter/chat_sub.dart
@@ -3,4 +3,6 @@ channel
   .subscribe(action: PresenceAction.enter)
   .listen((member) => ui.showMemberEntered(member));
 
-channel.subscribe().listen((message) => appendToConversation(message.data));
+channel
+  .subscribe()
+  .listen((message) => appendToConversation(message.data));
