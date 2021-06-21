766704ab15edc1b4013407e23ea9f78a77153e6c
sacOO7 <sachinshinde7676@gmail.com>
2021-06-21 18:54:28 +0530

    Updated map at places where hybrid values being sent

--- go/gps_pub.go
+++ go/gps_pub.go
@@ -1,2 +1,2 @@
 channel := client.Channels.Get("driver:elon")
-err = channel.Publish(context.Background(), "position", map[string]string{ "lat": 65, "long": 98, "bearing": "nw" })
+err = channel.Publish(context.Background(), "position", map[string]interface{}{ "lat": 65, "long": 98, "bearing": "nw" })
