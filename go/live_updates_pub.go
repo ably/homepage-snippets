8d3813dd8c91a09f45a7e3a11487618cc2d88920
sacOO7 <sachinshinde7676@gmail.com>
2021-06-21 18:54:28 +0530

    Updated map at places where hybrid values being sent

--- go/live_updates_pub.go
+++ go/live_updates_pub.go
@@ -1,2 +1,2 @@
 channel := client.Channels.Get("dogecoin:usd")
-channel.Publish(context.Background(), "rate", map[string]string{ "price": 2, "time": 543564332 })
+channel.Publish(context.Background(), "rate", map[string]int{ "price": 2, "time": 543564332 })
