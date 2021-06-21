dc77edc757e14141145bb81ac355c39a6d54b4ce
sacOO7 <sachinshinde7676@gmail.com>
2021-06-21 18:00:50 +0530

    Removed unnecessary client returned from newRealtime object

--- go/chat_pub.go
+++ go/chat_pub.go
@@ -1,4 +1,3 @@
-client, _ := ably.NewRealtime(ably.WithKey("ABLY_API_KEY"))
 channel := client.Channels.Get("room:tesla-fans")
 channel.Presence.Enter(context.Background(), UserName + " entered the channel")
 channel.Publish(context.Background(), "msg", map[string]string{"user": "elon", "message": "Hello fans"})
--- go/iot_pub.go
+++ go/iot_pub.go
@@ -1,3 +1,2 @@
-client, _ := ably.NewRealtime(ably.WithKey("ABLY_API_KEY"))
 channel := client.Channels.Get("device:rocket:1")
 channel.Publish(context.Background(), "action", map[string]string{ "task": "enable-thrusters" })
