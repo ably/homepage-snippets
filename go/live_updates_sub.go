f6bb95cce912f478fe5469d9dcdc5467b43ca925
sacOO7 <sachinshinde7676@gmail.com>
2021-06-23 01:51:41 +0530

    Removed unnecessary spec items as per ticket

--- go/live_updates_sub.go
+++
@@ -1,4 +0,0 @@
-// Subscribe to messages sent on the channel with given eventName
-channel.Subscribe(context.Background(), "rate", func(msg *ably.Message) {
-	appendMetric(msg.Data)
-})
--- go/multi_user_sub.go
+++
@@ -1,4 +0,0 @@
-// Subscribe to messages sent on the channel with given eventName
-channel.Subscribe(context.Background(), "update", func(msg *ably.Message) {
-	applyUpdate(msg.Data)
-})
