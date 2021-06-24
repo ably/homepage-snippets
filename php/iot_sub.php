c64a81eb45f3aece1cddd78a0e2c00c7425806a4
Tom Kirby-Green <tomkirbygreen@gmail.com>
2021-06-24 10:47:53 +0100

    Make function name more idiomatic.

--- php/iot_sub.php
+++ php/iot_sub.php
@@ -1,5 +1,5 @@
 $mqtt->subscribe('device:rocket:1', function ($topic, $message) {
     if ($topic == 'action') {
-       $rocket->processInstruction(json_decode($message));
+       $rocket->process_instruction(json_decode($message));
     }
 }, 0);
